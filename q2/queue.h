a6f5c409632d16ee1985c8be85e543ccd106c963
Mike Chaberski <mac937@nyu.edu>
2020-05-08 00:05:24 -0400

    use circular array implementation for queue

--- q2/queue.h
+++ q2/queue.h
@@ -8,17 +8,22 @@ const size_t VECTOR_QUEUE_RESET_CAPACITY = 10;
 
 /**
  * Queue implementation that uses a vector to store elements.
- * @tparam T
+ * This is a "circular array" implementation, in the Weiss terminology.
+ * @tparam T element type
  */
 template<class T>
 class VectorQueue {
 public:
-    VectorQueue() : elements_(), start_(0) {}
+    /**
+     * Constructs an instance.
+     */
+    VectorQueue() : elements_(), start_(0), last_(0), count_(0) {}
     /**
      * Adds an element to the queue.
      * @param element element
      */
     void Push(const T& element);
+
     /**
      * Removes an element from the queue.
      * @param valid flag that indicates whether removal succeeded; if queue is empty, this will be set to false
@@ -26,12 +31,21 @@ public:
      * a value constructed with the default construct of the element type
      */
     T Pop(bool& valid);
+
     /**
      * Removes an element from the queue.
      * @return the first element in the queue, or if the queue is empty,
      * a value constructed with the default construct of the element type
      */
     T Pop();
+
+    /**
+     * Returns a copy of the element at the front of the queue,
+     * but does not remove it. If the queue is empty, the value
+     * will be the default value for the type.
+     * @return element at the front of the queue
+     */
+    T Front() const;
 
     /**
      * Removes all elements from the queue.
@@ -50,23 +64,50 @@ public:
      */
     size_t Count() const;
 
+private:
+    std::vector<T> elements_;
+    size_t start_;
+    size_t last_;
+    size_t count_;
+    /**
+     * Compacts the vector used to store queue elements if this queue is empty.
+     */
+    void MaybeResize();
+    void NormalizeOrder();
+public: // stage: cut
+    /**
+     * Returns the size of the underlying vector.
+     * This is not necessarily the queue size.
+     * This function is used internally.
+     * @return size of underlying vector
+     */
+    size_t GetVectorSize() const;
+
+    /**
+     * Returns true if the size of the vector is equal to the queue size.
+     * @return true if packed
+     */
+    bool IsPacked() const;
+
     /**
      * Copies this queue to a vector.
      * @return a vector
      */
     std::vector<T> CopyToVector() const;
-private:
-    std::vector<T> elements_;
-    size_t start_;
-    /**
-     * Compacts the vector used to store queue elements if this queue is empty.
-     */
-    void MaybeResize();
+
 };
 
 template<class T>
 void VectorQueue<T>::Push(const T &element) {
-    elements_.push_back(element);
+    if (IsPacked()) {
+        NormalizeOrder();
+        elements_.push_back(element);
+        last_++;
+    } else {
+        elements_[last_ % elements_.size()] = element;
+        last_ = (last_ + 1) % elements_.size();
+    }
+    count_++;
 }
 
 template<class T>
@@ -76,7 +117,8 @@ T VectorQueue<T>::Pop(bool &valid) {
         return T();
     }
     T first = elements_[start_];
-    start_++;
+    start_ = (start_ + 1) % elements_.size();
+    count_--;
     MaybeResize();
     return first;
 }
@@ -85,6 +127,8 @@ template<class T>
 void VectorQueue<T>::Clear() {
     elements_.clear();
     start_ = 0;
+    last_ = 0;
+    count_ = 0;
     MaybeResize();
 }
 
@@ -100,12 +144,17 @@ std::vector<T> VectorQueue<T>::CopyToVector() const {
     for (size_t index = start_; index < elements_.size(); index++) {
         copy.push_back(elements_[index]);
     }
+    if (last_ <= start_) {
+        for (size_t index = 0; index < last_; index++) {
+            copy.push_back(elements_[index]);
+        }
+    }
     return copy;
 }
 
 template<class T>
 size_t VectorQueue<T>::Count() const {
-    return elements_.size() - start_;
+    return count_;
 }
 
 template<class T>
@@ -115,6 +164,7 @@ void VectorQueue<T>::MaybeResize() {
         elements_.shrink_to_fit();
         elements_.reserve(VECTOR_QUEUE_RESET_CAPACITY);
         start_ = 0;
+        last_ = 0;
     }
 }
 
@@ -122,6 +172,34 @@ template<class T>
 T VectorQueue<T>::Pop() {
     bool unused;
     return Pop(unused);
+}
+
+template<class T>
+bool VectorQueue<T>::IsPacked() const {
+    return GetVectorSize() == Count();
+}
+
+template<class T>
+size_t VectorQueue<T>::GetVectorSize() const {
+    return elements_.size();
+}
+
+template<class T>
+void VectorQueue<T>::NormalizeOrder() {
+    if (!IsEmpty() && last_ <= start_) {
+        std::vector<T> new_vector = CopyToVector();
+        elements_ = new_vector;
+        start_ = 0;
+        last_ = elements_.size();
+    }
+}
+
+template<class T>
+T VectorQueue<T>::Front() const {
+    if (IsEmpty()) {
+        return T();
+    }
+    T first = elements_[start_];
 }
 
 #endif //HW16_QUEUE_H
--- tests/queue_tests.cpp
+++ tests/queue_tests.cpp
@@ -8,14 +8,26 @@ TEST_CASE("Queue_push_one") {
     REQUIRE_FALSE(queue.IsEmpty());
     int count = queue.Count();
     REQUIRE(count == 1);
-    int val = queue.Pop();
+    int val = queue.Front();
+    REQUIRE(1 == val);
+    val = queue.Pop();
     REQUIRE(1 == val);
     count = queue.Count();
     REQUIRE(count == 0);
     REQUIRE(queue.IsEmpty());
 }
 
-TEST_CASE("Queue_clear") {
+TEST_CASE("Queue__IsPacked") {
+    VectorQueue<char> queue;
+    REQUIRE(queue.IsPacked());
+    queue.Push('a');
+    queue.Push('b');
+    REQUIRE(queue.IsPacked());
+    queue.Pop();
+    REQUIRE_FALSE(queue.IsPacked());
+}
+
+TEST_CASE("Queue_Clear") {
     VectorQueue<char> queue;
     queue.Push('a');
     queue.Push('b');
@@ -51,4 +63,34 @@ TEST_CASE("Queue_CopyToVector") {
     std::vector<int> expected({2, 3});
     std::vector<int> actual = queue.CopyToVector();
     REQUIRE(expected == actual);
+}
+
+TEST_CASE("Queue_useUnusedSlots") {
+    VectorQueue<char> queue;
+    queue.Push('a');
+    queue.Push('b');                    // a b
+    REQUIRE(2 == queue.Count());
+    REQUIRE('a' == queue.Pop());           // _ b
+    REQUIRE(1 == queue.Count());
+    std::vector<char> expected({'b'});
+    REQUIRE(expected == queue.CopyToVector());
+    queue.Push('c');                    // c b
+    expected = {'b', 'c'};
+    REQUIRE(expected == queue.CopyToVector());
+    REQUIRE(2 == queue.GetVectorSize());
+    REQUIRE(2 == queue.Count());
+    REQUIRE('b' == queue.Pop());           // c _
+    REQUIRE(1 == queue.Count());
+    queue.Push('d');                    // c d
+    REQUIRE(2 == queue.GetVectorSize());
+    REQUIRE(2 == queue.Count());
+    REQUIRE('c' == queue.Pop());           // _ d
+    REQUIRE(1 == queue.Count());
+    queue.Push('e');                    // e d
+    REQUIRE(2 == queue.GetVectorSize());
+    queue.Push('f');
+    expected = {'d', 'e', 'f'};
+    std::vector<char> actual = queue.CopyToVector();
+    REQUIRE(expected == actual);
+    REQUIRE(3 == queue.GetVectorSize());
 }
